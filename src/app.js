035aea710370e053b7a40052bb907e7d62874b69
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-12-13 16:20:02 +0530

    Add Express method

--- src/app.js
+++ src/app.js
@@ -10,4 +10,11 @@ app.use (cors({
 }))
 
 app.use (express.json({limit: "15kb"}))
+app.use(express.urlencoded ({extended : true, limit:"15kb"}))
+app.use (express.static("public"))
+
+
+
+
+
 export {app}
