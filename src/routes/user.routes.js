f2bf26b5d935fb782157e021f81aff09a07eba74
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-12-20 22:16:55 +0530

    Update routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,33 +1,30 @@
 import { Router } from "express";
-import {
-  loginUser,
-  logoutUser,
-  registerUser,
-} from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { logoutUser,loginUser,registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router();
+
+const router = Router()
 
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 2,
-    },
-    {
-      name: "coverImage",
-      maxCount: 2,
-    },
-  ]),
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
-  registerUser
-);
+router.route("/login").post(loginUser)
 
-router.route("/login").post(loginUser);
+//secured routes
+// logout routes
 
-//Secured routes
-//Routes for logout user
-router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
